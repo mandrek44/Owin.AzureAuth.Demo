c99e6a982fde18a5ff6149819ae2d1bbcfa5dfe7
Marcin Drobik <mdrobik@future-processing.com>
2015-01-05 16:20:26 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Owin.AzureAuth.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
